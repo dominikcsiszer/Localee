f8fd3a3c0f655f6457659e185de35c23727a1d8d
dominikcsiszer <dominikcsiszer@icloud.com>
2024-03-11 14:32:34 +0100

    Initial Commit

---
+++ Localee/LocaleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleeApp.swift
+//  Localee
+//
+//  Created by Csiszér Dominik on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocaleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
